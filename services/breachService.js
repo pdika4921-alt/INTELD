285516f80e595ddd8edfed65f1fc156c9dd51fd3
dika <pdika4921@gmail.com>
2026-08-21 16:32:20 +0700

    fix: LeakCheck header format

--- services/breachService.js
+++ services/breachService.js
@@ -281,4 +281,4 @@ async function runIntelligence(query, type) {
   return results;
 }
 
-module.exports = { runIntelligence, normalizePhone };
+module.exports = { runIntelligence, normalizePhone }};
